5f185e920e709824b5e7f6df23bd1a5cadedd1e7
VIKASH SINGH <rollinhigh4@gmail.com>
2020-05-29 17:17:54 +0530

    Add files via upload

--- src/main/java/com/vikash/mongo/MongoApplication.java
+++ src/main/java/com/vikash/mongo/MongoApplication.java
@@ -2,9 +2,11 @@ package com.vikash.mongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MongoApplication {
+public class MongoApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongoApplication.class, args);
