8fc28b841beed3ed3479ef7919bb7c410150d0af
VIKASH SINGH <rollinhigh4@gmail.com>
2020-05-29 18:05:33 +0530

    Update MongoApplication.java

--- src/main/java/com/vikash/mongo/MongoApplication.java
+++ src/main/java/com/vikash/mongo/MongoApplication.java
@@ -1,15 +1,26 @@
 package com.vikash.mongo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class MongoApplication  {
+public class MongoApplication extends SpringBootServletInitializer  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongoApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MongoApplication.class);
+	}
 }
+
